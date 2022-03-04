50c337785ccaa437a42824184f7e42ff2b8cf6ba
Kanishk Gohil <35946818+kgohil27@users.noreply.github.com>
2022-03-04 11:22:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,6 @@ setup(
         "numba==0.45.1",
         "numpy==1.17.3",
         "pandas==0.25.2",
-        "re==2.2.1",
         "scikit-learn==0.21.3",
         "scipy==1.3.1",
         "seaborn==0.11.2",
