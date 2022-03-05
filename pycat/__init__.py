a3a35b7cd913020e7269735515a38bf2000311df
Kanishk Gohil <35946818+kgohil27@users.noreply.github.com>
2022-03-04 19:42:31 -0500

    Update and rename PyCAT/__init__.py to pycat/__init__.py

--- pycat/__init__.py
+++ pycat/__init__.py
@@ -6,6 +6,8 @@ of aerosols from their size-resolved number concentration measurements. CCN
 activity analysis performed using Kohler framework.
 """
 
+from .version import __version__
+
 __author__ = "Kanishk Gohil <kgohil@umd.edu>"
 
 from .Datafile_preprocessing import *
